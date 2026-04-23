4d251e206bddc095da4ef7c1b16b0aca65c451e6
Mostafa-Elabsawy <mostfaelabsawy@gmail.com>
2026-04-24 02:19:01 +0300

    edited home page

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { ButtonDirective, ButtonModule } from "primeng/button";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonDirective,ButtonModule],
+  imports: [ButtonDirective, ButtonModule, CommonModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  navbarCollapsed=signal<boolean>(true);
+
+  toggleNavbar() {
+    this.navbarCollapsed.set(!this.navbarCollapsed());
+  }
 
 }
